5950a8c6e6d35af7fc2150f54c0db6c2f7159881
knarvorn <knarvorn30@gmail.com>
2016-06-27 21:20:34 +0200

    luuuooooooops

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,29 +1,22 @@
-object Main extends App {
-  //John.svar("fisk")
-  //John.svar("bi")
-  //John.svar("gud")
-  //John.svar("hugo")
-  //John.flyv("lavt")
-  //Bias.flyv("højt")
-  //Bias.slå(John)
-  fister.løb("hurtigt")
-}
 class Person(val navn: String) {
-  def flyv(højde: String): Unit = {
-    println(navn + " flyver " + højde)
+
+  val titel = "Hr. " + navn
+  var løbet = 0
+  var flyvet = 0
+
+  def flyv(fart: Int): Unit = {
+    println(navn + " flyver " + fart)
+    flyvet = fart + flyvet
   }
   def slå(person: Person) = {
     println(this.navn + " slog " + person.navn)
   }
+  def løb(længde: Int) = {
+    println(this.navn + " løb " + længde + " meter")
+    løbet += længde
+  }
 }
-def løb(længde: person9) = {
-  println(navn.person + " løb " længde)
-}
-object Bias extends Person("Bias") {
-}
-object fister extends Person("fister") {
 
-}
 object John extends Person ("John") {
 
   val ynglingsord = "fisk"
@@ -42,4 +35,33 @@ object John extends Person ("John") {
       println("Jeg har ikke gået i skole nok til at kende det ord")
     }
   }
+}
+
+val bias = new Person("Bias")
+
+println("Jeg hedder " +  bias.titel)
+bias.løb(6)
+bias.løb(5)
+println (bias.titel + " har i alt løbet " + bias.løbet + " meter")
+bias.flyv(7)
+bias.flyv(1)
+println (bias.titel + " har i alt flyvet " + bias.flyvet + " meter")
+
+
+var i = 0
+while (i <= 100){
+  if ((i % 3 ) == 0 && (i % 5 ) == 0 ){
+    println("baaaaaaannngbuuuiiim")
+  }
+  else if ((i % 3) == 0 ){
+    println ("baaaaaaannng")
+  }
+  else if ((i % 5) == 0 ) {
+    println ("buuuuuuuiiiim")
+  }
+
+  else {
+    println (i)
+  }
+   i += 1
 }
